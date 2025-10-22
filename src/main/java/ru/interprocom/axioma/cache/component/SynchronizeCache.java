03e2586554377a973ff369cf6365baa3daf244f3
ogoncharenko <ogoncharenko@interprocom.ru>
2025-10-22 09:44:41 +0300

    Запуск bean'а по синхронизации для всех профилей кроме test'а

--- src/main/java/ru/interprocom/axioma/cache/component/SynchronizeCache.java
+++ src/main/java/ru/interprocom/axioma/cache/component/SynchronizeCache.java
@@ -6,6 +6,7 @@ import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import ru.interprocom.axioma.cache.annotation.AxiCache;
@@ -15,19 +16,20 @@ import java.util.*;
 
 @Slf4j
 @Component
+@Profile("!test")
 public class SynchronizeCache implements ApplicationContextAware {
 	private static final ArrayList<Object> cacheClasses = new ArrayList<>();
 	private ApplicationContext applicationContext;
 
 	@PostConstruct
-	public void loadToCacheDB() {
+	private void loadToCacheDB() {
 		cacheClasses.addAll(applicationContext.getBeansWithAnnotation(AxiCache.class).values());
 		cacheClasses.forEach(aClass -> {((AxiomaCache) aClass).load();});
 	}
 
 	@Scheduled(fixedDelayString = "PT30M")
 	@SchedulerLock(name = "syncCache", lockAtLeastFor = "PT30M", lockAtMostFor = "PT30M")
-	public void syncCache() {
+	private void syncCache() {
 		cacheClasses.forEach(aClass -> {((AxiomaCache) aClass).sync();});
 	}
 
